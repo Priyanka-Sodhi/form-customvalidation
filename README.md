8af6c357025e37e4e93196b92b9262051d1c3cae
Priyanka-Sodhi <priyankasodhi00@gmail.com>
2020-07-25 22:45:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# form-customvalidation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/form-customvalidation)
---
+++ src/app/customvalidations/customvalidations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customvalidations',
+  templateUrl: './customvalidations.component.html',
+  styleUrls: ['./customvalidations.component.css']
+})
+export class CustomvalidationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
